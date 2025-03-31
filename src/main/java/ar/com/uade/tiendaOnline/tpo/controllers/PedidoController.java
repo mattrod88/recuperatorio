e05091d243b5712c07eea433d31066b9df994ea2
Daniela <dggtn97@gmail.com>
2025-03-31 19:30:59 -0300

    agrego los metodos getCategorias, getCategoriasById y crearCategoria+Excepcion

--- src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/PedidoController.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/PedidoController.java
@@ -1,4 +1,15 @@
 package ar.com.uade.tiendaOnline.tpo.controllers;
 
+
+import ar.com.uade.tiendaOnline.tpo.servicios.pedido.IPedidoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/productos")
+@CrossOrigin
 public class PedidoController {
+    private IPedidoServicio pedidoServicio;
 }
