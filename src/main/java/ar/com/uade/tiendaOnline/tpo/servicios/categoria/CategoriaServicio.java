69bff40d284e978d8ceb466c032b3d00f0cc8166
Daniela <dggtn97@gmail.com>
2025-03-31 19:44:55 -0300

    agrego CategoriaDuplicateExcepcion

---
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/categoria/CategoriaServicio.java
@@ -0,0 +1,33 @@
+package ar.com.uade.tiendaOnline.tpo.servicios.categoria;
+import ar.com.uade.tiendaOnline.tpo.entidad.Categoria;
+import ar.com.uade.tiendaOnline.tpo.excepciones.CategoriaDuplicateExcepcion;
+import ar.com.uade.tiendaOnline.tpo.repositorio.CategoriaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoriaServicio implements ICategoriaServicio {
+        @Autowired
+        private CategoriaRepositorio categoriaRepositorio;
+
+        public Page<Categoria> getCategorias(PageRequest pageable) {
+            return categoriaRepositorio.findAll(pageable);
+        }
+
+        public Optional<Categoria> getCategoriaById(Long categoryId) {
+            return categoriaRepositorio.findById(categoryId);
+        }
+
+        public Categoria crearCategoria(String descripcion) throws CategoriaDuplicateExcepcion {
+            List<Categoria> categorias = categoriaRepositorio.findByDescripcion(descripcion);
+            if (categorias.isEmpty())
+                return categoriaRepositorio.save(new Categoria(descripcion));
+            throw new CategoriaDuplicateExcepcion();
+        }
+
+}
