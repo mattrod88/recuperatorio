c55fdb57c2e13bd6fe8a021c718625ee5a82a41e
Daniela <dggtn97@gmail.com>
2025-03-31 21:57:42 -0300

    creo la clase PedidoController y agrego como atributo la interfaz IpedidoServicio

--- src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/PedidoController.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/PedidoController.java
@@ -1,15 +1,23 @@
 package ar.com.uade.tiendaOnline.tpo.controllers;
 
 
+
+import ar.com.uade.tiendaOnline.tpo.entidad.Cliente;
+import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
+
 import ar.com.uade.tiendaOnline.tpo.servicios.pedido.IPedidoServicio;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/productos")
+@RequestMapping("pedidos")
 @CrossOrigin
 public class PedidoController {
+    @Autowired
     private IPedidoServicio pedidoServicio;
+
+
 }
