fc6db1168a1482f0f1e0cd099bcccaddf8d9ba96
Daniela <dggtn97@gmail.com>
2025-03-31 22:10:22 -0300

    clase implementa de su interfaz y tiene metodo obtenerPedidoPorCliente

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/pedido/PedidoServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/pedido/PedidoServicio.java
@@ -1,4 +1,19 @@
 package ar.com.uade.tiendaOnline.tpo.servicios.pedido;
 
-public class PedidoServicio {
+import ar.com.uade.tiendaOnline.tpo.entidad.Cliente;
+import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
+import ar.com.uade.tiendaOnline.tpo.repositorio.PedidoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PedidoServicio implements IPedidoServicio{
+    @Autowired
+    private PedidoRepositorio pedidoRepositorio;
+
+    public List<Pedido> obtenerPedidoPorCliente(Cliente cliente){
+        return pedidoRepositorio.obtenerPorCliente(cliente.getId());
+    }
 }
