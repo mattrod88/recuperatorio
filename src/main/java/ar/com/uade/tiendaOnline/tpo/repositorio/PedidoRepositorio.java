4f3bad81a6758e35822e8554726e6787c5f4d799
Daniela <dggtn97@gmail.com>
2025-03-31 22:04:26 -0300

    creo query para encontrar pedido segun id de cliente

--- src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/PedidoRepositorio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/PedidoRepositorio.java
@@ -1,4 +1,14 @@
 package ar.com.uade.tiendaOnline.tpo.repositorio;
 
-public interface PedidoRepositorio {
+import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PedidoRepositorio extends JpaRepository<Pedido,Long> {
+    @Query(value = "select p from Pedido p where p.cliente.id = ?1")
+    List<Pedido> obtenerPorCliente(Long id);
 }
