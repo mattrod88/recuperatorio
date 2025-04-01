6e5b89ee4a58257d3bb085c194cf6ca2b49b9775
Daniela <dggtn97@gmail.com>
2025-03-31 22:05:03 -0300

    hago que interfaz extienda de JpaRepository con sus atributos

--- src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/CarritoRepositorio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/CarritoRepositorio.java
@@ -1,4 +1,7 @@
 package ar.com.uade.tiendaOnline.tpo.repositorio;
 
-public interface CarritoRepositorio {
+import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarritoRepositorio extends JpaRepository<Pedido,Long> {
 }
