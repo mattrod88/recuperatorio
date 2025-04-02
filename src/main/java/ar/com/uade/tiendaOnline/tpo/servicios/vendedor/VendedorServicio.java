a8f4e73634385fe91c615b840d96e1843660d9fe
Daniela <dggtn97@gmail.com>
2025-04-02 17:51:01 -0300

    hago que el metodo obtenerVentas devuelva un findAll de vendedorRepositorio

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/vendedor/VendedorServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/vendedor/VendedorServicio.java
@@ -2,13 +2,23 @@ package ar.com.uade.tiendaOnline.tpo.servicios.vendedor;
 
 
 
+import ar.com.uade.tiendaOnline.tpo.entidad.Cliente;
+import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
+import ar.com.uade.tiendaOnline.tpo.repositorio.ProductoRepositorio;
+import ar.com.uade.tiendaOnline.tpo.repositorio.VendedorRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 
 @Service
 public class VendedorServicio implements  IVendedorServicio{
-
+    @Autowired
+    private VendedorRepositorio vendedorRepositorio;
+    public List<Pedido> obtenerVentas(){
+        return vendedorRepositorio.findAll();
+    }
 
 
 
