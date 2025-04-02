3f243adc6bcc6f55423ab623de540d06f781c05b
Daniela <dggtn97@gmail.com>
2025-04-02 17:23:34 -0300

    agrego un dto para la clase Vendedor

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/VendedorRequest.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/VendedorRequest.java
@@ -1,4 +1,17 @@
 package ar.com.uade.tiendaOnline.tpo.entidad.dto;
 
+import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
+import ar.com.uade.tiendaOnline.tpo.entidad.Rol;
+import jakarta.persistence.*;
+
+import java.util.List;
+
 public class VendedorRequest {
+    private Long id;
+    private String email;
+    private String nombre;
+    private String nombreEmprendimiento;
+    private String apellido;
+    private List<Pedido> pedidos;
+    private List<Rol> roles;
 }
