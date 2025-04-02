01c49b816394300d959ae39db68c2f9bf946bbf1
Daniela <dggtn97@gmail.com>
2025-04-02 17:24:26 -0300

    agreg una query para mostrar detalle de compra segun id de cliente

--- src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/DetalleCompraRepositorio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/DetalleCompraRepositorio.java
@@ -1,7 +1,15 @@
 package ar.com.uade.tiendaOnline.tpo.repositorio;
 
+import ar.com.uade.tiendaOnline.tpo.entidad.DetalleDeCompra;
 import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
+import ar.com.uade.tiendaOnline.tpo.entidad.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface DetalleCompraRepositorio extends JpaRepository<Pedido,Long> {
+    @Query(value = "select d from DetalleDeCompra d where d.cliente.id = ?1")
+    List<DetalleDeCompra> findByIdCliente(Long id)  ;
 }
