441a43d815d13db5e0ae4b4734e50792a6c9d31c
Daniela <dggtn97@gmail.com>
2025-04-02 17:14:12 -0300

    agrego sus atributos y una relacion con cliente id asi puedo buscar el detalleDeCompra por el id del cliente

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/DetalleDeCompra.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/DetalleDeCompra.java
@@ -1,4 +1,33 @@
 package ar.com.uade.tiendaOnline.tpo.entidad;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
 public class DetalleDeCompra {
+    @Column
+    private Date fecha;
+    @Column
+    private double montoTotal;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", referencedColumnName = "id")
+    private Cliente cliente;
+
+
+    public DetalleDeCompra(double montoTotal, long id, Cliente cliente) {
+        this.montoTotal = montoTotal;
+        this.id = id;
+        this.cliente = cliente;
+
+    }
+
+    public DetalleDeCompra() {
+    }
 }
