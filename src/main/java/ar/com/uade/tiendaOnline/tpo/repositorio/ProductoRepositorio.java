89726fb96979d8cf3a944cd3208ab69aa2513b07
Daniela <dggtn97@gmail.com>
2025-04-02 10:15:22 -0300

    agrego una query para buscar el producto segun su categoria

--- src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/ProductoRepositorio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/ProductoRepositorio.java
@@ -3,11 +3,13 @@ package ar.com.uade.tiendaOnline.tpo.repositorio;
 import ar.com.uade.tiendaOnline.tpo.entidad.Categoria;
 import ar.com.uade.tiendaOnline.tpo.entidad.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ProductoRepositorio extends JpaRepository<Producto,Long> {
-    List<Producto> findByCategoria(Categoria categoria) ;
+    @Query(value = "select p from Producto p where p.categoria.descripcion = ?1")
+    List<Producto> findByCategoria(String categoria) ;
 }
