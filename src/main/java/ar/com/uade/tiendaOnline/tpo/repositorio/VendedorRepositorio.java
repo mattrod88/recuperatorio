79ecc855e270a1d4c494c4e68178d83a0c6b2fe7
Daniela <dggtn97@gmail.com>
2025-04-02 17:45:17 -0300

    creo interfaz

--- src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/VendedorRepositorio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/VendedorRepositorio.java
@@ -1,7 +1,11 @@
 package ar.com.uade.tiendaOnline.tpo.repositorio;
 
 import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
+import ar.com.uade.tiendaOnline.tpo.entidad.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface VendedorRepositorio extends JpaRepository<Pedido,Long> {
