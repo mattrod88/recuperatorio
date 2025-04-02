a5f1bb4f1a9f9f50de76ae65af52b0489c24c476
Daniela <dggtn97@gmail.com>
2025-04-02 17:22:55 -0300

    agrego un dto para la clase DetalleDeCompra

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/DetalleDeCompraRequest.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/DetalleDeCompraRequest.java
@@ -1,4 +1,16 @@
 package ar.com.uade.tiendaOnline.tpo.entidad.dto;
 
+import ar.com.uade.tiendaOnline.tpo.entidad.Cliente;
+import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
+import lombok.Data;
+
+
+import java.util.Date;
+import java.util.List;
+
+@Data
 public class DetalleDeCompraRequest {
+    private double montoTotal;
+    private long id;
+    private Date fecha;
 }
