9bb5f867cfb27ab3a853d909bab67336cf2122a1
Daniela <dggtn97@gmail.com>
2025-04-02 17:27:26 -0300

    hago que la clase extienda de jpa

--- src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/VendedorRepositorio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/VendedorRepositorio.java
@@ -2,9 +2,7 @@ package ar.com.uade.tiendaOnline.tpo.repositorio;
 
 import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
 
 public interface VendedorRepositorio extends JpaRepository<Pedido,Long> {
 
