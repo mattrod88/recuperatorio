76de3aa433be3cb14dcfcc7292709c1f82ba1415
Daniela <dggtn97@gmail.com>
2025-04-02 17:29:06 -0300

    creo interfaz y sus metodos

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/detalleDeCompra/IDetalleDeCompraServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/detalleDeCompra/IDetalleDeCompraServicio.java
@@ -1,4 +1,11 @@
 package ar.com.uade.tiendaOnline.tpo.servicios.detalleDeCompra;
 
-public interface IDetalleDeCompra {
+import ar.com.uade.tiendaOnline.tpo.entidad.DetalleDeCompra;
+import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IDetalleDeCompraServicio {
+   List<DetalleDeCompra> mostraDetalleCompra(Long id);
 }
