21da305bf66bf41234d9d2ca24ebbf3f39f52949
Daniela <dggtn97@gmail.com>
2025-04-06 10:21:02 -0300

    creamos la interfaz ICarrito en servicios para el modelo de negocio

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/carrito/ICarrito.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/carrito/ICarrito.java
@@ -1,4 +1,12 @@
 package ar.com.uade.tiendaOnline.tpo.servicios.carrito;
 
+import ar.com.uade.tiendaOnline.tpo.entidad.ItemCarrito;
+
+
+
 public interface ICarrito {
+
+    void calcularTotal() ;
+    void agregarItem(ItemCarrito item);
+    void eliminarItem(ItemCarrito item);
 }
