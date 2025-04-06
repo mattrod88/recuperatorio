470ed06cedc3f9167cad329a486d0c35ff57cb00
KeilaCheca <kchecaorihuela@uade.edu.ar>
2025-04-06 18:02:37 -0300

    Método que obtiene pedidos paginados o todos si no se indica página y tamaño

--- src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/PedidoController.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/PedidoController.java
@@ -8,6 +8,8 @@ import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
 import ar.com.uade.tiendaOnline.tpo.servicios.pedido.IPedidoServicio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +18,18 @@ import java.util.List;
 @RequestMapping("pedidos")
 @CrossOrigin
 public class PedidoController {
+
     @Autowired
     private IPedidoServicio pedidoServicio;
+
+    @GetMapping
+    public ResponseEntity<Page<Pedido>> getPedidos(
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) Integer size) {
+        if (page == null || size == null)
+            return ResponseEntity.ok(pedidoServicio.getPedidos(PageRequest.of(0, Integer.MAX_VALUE)));
+        return ResponseEntity.ok(pedidoServicio.getPedidos(PageRequest.of(page, size)));
+    }
 
 
 }
