a6d5f99917be701a36951c76d39b9aa218f28c6f
KeilaCheca <kchecaorihuela@uade.edu.ar>
2025-04-06 18:25:04 -0300

    agregue el metodo para obtener todos los pedidos con paginacion

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/pedido/PedidoServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/pedido/PedidoServicio.java
@@ -4,6 +4,7 @@ import ar.com.uade.tiendaOnline.tpo.entidad.Cliente;
 import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
 import ar.com.uade.tiendaOnline.tpo.repositorio.PedidoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,10 @@ import java.util.List;
 public class PedidoServicio implements IPedidoServicio{
     @Autowired
     private PedidoRepositorio pedidoRepositorio;
+
+    public Page<Pedido> getPedidos(PageRequest pageable){
+        return pedidoRepositorio.findAll(pageable);
+    }
 
     public List<Pedido> obtenerPedidoPorCliente(Cliente cliente){
         return pedidoRepositorio.obtenerPorCliente(cliente.getId());
