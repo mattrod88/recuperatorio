338442cd89135e229b3d5247d25e28e5bcab1574
Daniela <dggtn97@gmail.com>
2025-04-06 10:18:32 -0300

    creamos el dto para carrito

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/CarritoRequest.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/CarritoRequest.java
@@ -1,4 +1,19 @@
 package ar.com.uade.tiendaOnline.tpo.entidad.dto;
+import ar.com.uade.tiendaOnline.tpo.entidad.Cliente;
+import ar.com.uade.tiendaOnline.tpo.entidad.ItemCarrito;
+import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class CarritoRequest {
+    private Long id;
+
+    private Cliente cliente;
+
+    private List<ItemCarrito> items = new ArrayList<>();
+
+    private double total;
+
 }
