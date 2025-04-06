aa1a325971f9302a20714acb265ce6dda4293784
Daniela <dggtn97@gmail.com>
2025-04-06 10:19:30 -0300

    creamos dto para ItemCarrito

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/ItemCarritoRequest.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/ItemCarritoRequest.java
@@ -1,4 +1,15 @@
 package ar.com.uade.tiendaOnline.tpo.entidad.dto;
 
+import ar.com.uade.tiendaOnline.tpo.entidad.Carrito;
+import ar.com.uade.tiendaOnline.tpo.entidad.Producto;
+
 public class ItemCarritoRequest {
+    private Long id;
+
+    private Producto producto;
+
+
+    private Carrito carrito;
+
+    private int cantidad;
 }
