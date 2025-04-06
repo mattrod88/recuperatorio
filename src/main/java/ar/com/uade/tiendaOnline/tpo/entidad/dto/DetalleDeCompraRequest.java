20202ffa5be6d19402b3a8f69d44d81a9a56beee
Daniela <dggtn97@gmail.com>
2025-04-06 10:19:00 -0300

    creamo dto para detalle de compra

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/DetalleDeCompraRequest.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/DetalleDeCompraRequest.java
@@ -1,12 +1,8 @@
 package ar.com.uade.tiendaOnline.tpo.entidad.dto;
-
-import ar.com.uade.tiendaOnline.tpo.entidad.Cliente;
-import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
 import lombok.Data;
 
 
 import java.util.Date;
-import java.util.List;
 
 @Data
 public class DetalleDeCompraRequest {
