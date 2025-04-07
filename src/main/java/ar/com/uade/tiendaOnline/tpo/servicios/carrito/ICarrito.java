873651d6dee1900ee1f2269b7e6795ec1505f15a
Daniela <dggtn97@gmail.com>
2025-04-07 11:37:10 -0300

    comento los metodos que se pueden agregar a la interfaz de carrito

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/carrito/ICarrito.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/carrito/ICarrito.java
@@ -9,4 +9,12 @@ public interface ICarrito {
     void calcularTotal() ;
     void agregarItem(ItemCarrito item);
     void eliminarItem(ItemCarrito item);
+
+    //modificarCantidad():
+    //Los clientes pueden modificar la cantidad de productos en su carrito, es decir, aumentar o disminuir la cantidad de cada producto seleccionado.
+    //checkout():
+    //El carrito tiene un total que se calcula automáticamente, teniendo en cuenta el precio de cada producto y la cantidad.
+    //Al realizar el checkout, el sistema valida que haya suficiente stock disponible para cada producto del carrito.
+    //descontarStock():
+    //Después de que el cliente confirme el checkout y el pedido sea procesado, el sistema actualizará el stock de los productos comprados, descontando la cantidad correspondiente del inventario.
 }
