c531649052d7d7ae7d29072ab08016049c9b06d3
Daniela <dggtn97@gmail.com>
2025-04-07 11:34:02 -0300

    comente algunos metodos que se pueden agregar a la interfaz de VendedorServicio

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/vendedor/IVendedorServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/vendedor/IVendedorServicio.java
@@ -6,4 +6,12 @@ import java.util.List;
 
 public interface IVendedorServicio {
     public List<Pedido> obtenerVentas();
+
+
+    //darDeAltaProducto(Long id):
+    //Permite al vendedor dar de alta un nuevo producto. El producto debe incluir la siguiente información:
+    //Nombre,Descripción,Categoría,Precio,Foto (imagen)
+    //modificarProducto(Long id):
+    //eliminarProducto(Long id):
+    //agregarProducto(Long id):
 }
