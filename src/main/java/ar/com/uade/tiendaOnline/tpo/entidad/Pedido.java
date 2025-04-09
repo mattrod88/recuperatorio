b1921cd9a6bbad26a182028e154f364d3f9e87ce
Daniela <dggtn97@gmail.com>
2025-04-09 20:04:15 -0300

    borro las relaciones que tenia pedido dentro

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/Pedido.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/Pedido.java
@@ -4,10 +4,6 @@ import java.time.LocalDate;
 
 import jakarta.persistence.*;
 import lombok.Data;
-//puede tener una lista con producto ,su cantidad y precio unitario
-//pedido va con vendedor y cliente
-//1 pedido 1 cliente
-//1 pedido 1 vendedor
 @Entity
 @Data
 @Table(name = "pedidos")
@@ -19,13 +15,10 @@ public class Pedido {
     @Column
     private Long count;
 
-//    @ManyToOne
-//    @JoinColumn(name = "cliente_id",nullable = false)
-//    private Cliente cliente;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "vendedor_id",nullable = false)
-//    private Vendedor vendedor;
+    @ManyToOne
+    @JoinColumn(name = "usuario_id",nullable = false)
+    private Usuario usuario;
+
 
     @Column(name = "fecha")
     private LocalDate fecha;
@@ -37,12 +30,11 @@ public class Pedido {
     private double total;
 
 
-//    public Pedido(Long id, Long count, Cliente cliente, Vendedor vendedor) {
-//        this.id = id;
-//        this.count = count;
-//        this.cliente = cliente;
-//        this.vendedor = vendedor;
-//    }
+    public Pedido(Long id, Long count,Usuario usuario) {
+        this.id = id;
+        this.count = count;
+        this.usuario=usuario;
+    }
 
     public Pedido() {
     }
