15c1ea4d83964de2b45061f7abf4dca48d00401f
Daniela <dggtn97@gmail.com>
2025-04-09 20:21:40 -0300

    agrego los metodos registrarse,logearse y obterneTodosLosUsuarios

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/usuario/IUsuarioServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/usuario/IUsuarioServicio.java
@@ -1,4 +1,15 @@
 package ar.com.uade.tiendaOnline.tpo.servicios.usuario;
 
+import ar.com.uade.tiendaOnline.tpo.entidad.Usuario;
+import ar.com.uade.tiendaOnline.tpo.entidad.dto.UsuarioDTO;
+
+
+import java.util.List;
+
 public interface IUsuarioServicio {
+    void registrarse(Usuario usuario);
+
+    void logearse(Usuario usuario);
+
+   List<UsuarioDTO> obtenerTodosLosUsuarios();
 }
