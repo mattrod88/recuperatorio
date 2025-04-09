a301f3bdbc9e15f8e1a5e6b232648265fb7c2ed9
Daniela <dggtn97@gmail.com>
2025-04-09 08:57:04 -0300

    creo el dto de pedido como PedidoRequest

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/PedidoRequest.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/PedidoRequest.java
@@ -1,4 +1,18 @@
 package ar.com.uade.tiendaOnline.tpo.entidad.dto;
 
+import ar.com.uade.tiendaOnline.tpo.entidad.Cliente;
+import ar.com.uade.tiendaOnline.tpo.entidad.Vendedor;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
 public class PedidoRequest {
+    private Long id;
+    private Long count;
+    private Cliente cliente;
+    private Vendedor vendedor;
+    private LocalDate fecha;
+    private String estado;
+    private double total;
+
 }
