37584c37e2b95f0d984125966911e030eab127cb
Daniela <dggtn97@gmail.com>
2025-04-09 20:16:08 -0300

    creo excepcion para no permitir registrar usuarios con un mismo mail y que aparezca el mensaje

--- src/main/java/ar/com/uade/tiendaOnline/tpo/excepciones/UsuarioDuplicado.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/excepciones/UsuarioDuplicado.java
@@ -1,7 +1,12 @@
 package ar.com.uade.tiendaOnline.tpo.excepciones;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "El mail que se intenta agregar ya esta registrado")
+
 public class UsuarioDuplicado extends RuntimeException {
-  public UsuarioDuplicado(String message) {
-    super(message);
-  }
-}
+
+    }
+
+
