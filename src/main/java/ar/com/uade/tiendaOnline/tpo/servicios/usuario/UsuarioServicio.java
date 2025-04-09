0266a8956950757e834d0353e9d84d5316f7eea5
Daniela <dggtn97@gmail.com>
2025-04-09 20:23:15 -0300

    metodos para la la clase UsuarioServicio uno que obtenga todos los usuarios y que de esa lista convierta cada elemento(cada usuario ) en un usuario de tipo dto asi cuando retorna mustra solo los atributos que queremos ver de cada usuario registrado sin ver la contrasena

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/usuario/UsuarioServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/usuario/UsuarioServicio.java
@@ -1,4 +1,54 @@
-package ar.com.uade.tiendaOnline.tpo.servicios;
+package ar.com.uade.tiendaOnline.tpo.servicios.usuario;
 
-public class UsuarioServicio {
+
+import ar.com.uade.tiendaOnline.tpo.entidad.Roles;
+import ar.com.uade.tiendaOnline.tpo.entidad.Usuario;
+import ar.com.uade.tiendaOnline.tpo.entidad.dto.UsuarioDTO;
+import ar.com.uade.tiendaOnline.tpo.excepciones.CategoriaDuplicateExcepcion;
+import ar.com.uade.tiendaOnline.tpo.excepciones.ProductoInexistenteExcepcion;
+import ar.com.uade.tiendaOnline.tpo.excepciones.UsuarioDuplicado;
+import ar.com.uade.tiendaOnline.tpo.repositorio.UsuarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class UsuarioServicio implements IUsuarioServicio{
+    @Autowired
+    private UsuarioRepositorio usuarioRepositorio;
+
+    public void registrarse(Usuario usuario){
+        try {
+            usuario.setRol(Roles.CLIENTE);
+            usuarioRepositorio.save(usuario);
+        }catch (DataIntegrityViolationException e) {
+            throw new UsuarioDuplicado();
+        }
+
+    }
+
+    @Override
+    public void logearse(Usuario usuario) {
+
+    }
+
+    @Override
+    public List<UsuarioDTO> obtenerTodosLosUsuarios() {
+        List<Usuario>usuarios =  usuarioRepositorio.findAll();
+        List<UsuarioDTO>usuariosDto=  new LinkedList<>();
+        for(Usuario usuario :usuarios){
+            UsuarioDTO udto = new UsuarioDTO();
+            udto.setNombre(usuario.getNombre());
+            udto.setEmail(usuario.getEmail());
+            udto.setApellido(usuario.getApellido());
+            udto.setId(usuario.getId());
+            udto.setRol(usuario.getRol().name());
+            usuariosDto.add(udto);
+        }
+        return usuariosDto;
+    }
 }
