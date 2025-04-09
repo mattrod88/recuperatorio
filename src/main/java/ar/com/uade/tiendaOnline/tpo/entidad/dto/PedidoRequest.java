69c84167c4f1b55af0861050fc5f3f900f36e9d2
Daniela <dggtn97@gmail.com>
2025-04-09 20:08:28 -0300

    todos sus atributos

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/PedidoRequest.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/PedidoRequest.java
@@ -1,12 +1,13 @@
 package ar.com.uade.tiendaOnline.tpo.entidad.dto;
 
+import ar.com.uade.tiendaOnline.tpo.entidad.Usuario;
+
 import java.time.LocalDate;
 
 public class PedidoRequest {
     private Long id;
     private Long count;
-    private Cliente cliente;
-    private Vendedor vendedor;
+    private Usuario usuario;
     private LocalDate fecha;
     private String estado;
     private double total;
