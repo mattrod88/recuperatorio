642ca79778f6312bf27053306c10988893eb278b
Daniela <dggtn97@gmail.com>
2025-04-09 20:14:38 -0300

    creo usuario dto para que solo se muestren ciertos datos del usuario registrado cuando hago la peticion de get usuarios y asi no me aparece la clave del usuario

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/UsuarioDTO.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/UsuarioDTO.java
@@ -1,4 +1,14 @@
 package ar.com.uade.tiendaOnline.tpo.entidad.dto;
 
-public class UsuarioRequest {
+import ar.com.uade.tiendaOnline.tpo.entidad.Roles;
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
+public class UsuarioDTO {
+    private Long id;
+    private String email;
+    private String nombre;
+    private String apellido;
+    private String rol;
 }
