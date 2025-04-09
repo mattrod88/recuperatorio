f7a257e35ccffb89537d32dfb9d7bb1bae83b422
Daniela <dggtn97@gmail.com>
2025-04-09 20:20:51 -0300

    creo la interfaz UsuarioRepositorio

--- src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/UsuarioRepositorio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/UsuarioRepositorio.java
@@ -1,4 +1,9 @@
 package ar.com.uade.tiendaOnline.tpo.repositorio;
 
-public interface UsuarioRepositorio {
+import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
+import ar.com.uade.tiendaOnline.tpo.entidad.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepositorio extends JpaRepository<Usuario,Long> {
+
 }
