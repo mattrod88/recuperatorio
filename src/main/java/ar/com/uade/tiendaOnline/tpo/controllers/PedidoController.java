781b87ab5a8249df3dfde8544f4c2ad5dbb3ea52
Daniela <dggtn97@gmail.com>
2025-04-10 14:35:28 -0300

    dejo metodo realizar compra

--- src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/PedidoController.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/PedidoController.java
@@ -1,55 +1,31 @@
 package ar.com.uade.tiendaOnline.tpo.controllers;
 
 import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
+import ar.com.uade.tiendaOnline.tpo.entidad.dto.ItemPedidoDTO;
+import ar.com.uade.tiendaOnline.tpo.entidad.dto.PedidoDTO;
 import ar.com.uade.tiendaOnline.tpo.servicios.pedido.IPedidoServicio;
 
+import ar.com.uade.tiendaOnline.tpo.servicios.pedido.PedidoServicio;
 import org.springframework.data.domain.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("pedidos")
-@CrossOrigin
-public class PedidoController {
+    @RestController
+    @CrossOrigin
+    @RequestMapping("/api/pedidos")
+    public class PedidoController {
 
-    @Autowired
-    private IPedidoServicio pedidoServicio;
+        @Autowired
+        private PedidoServicio pedidoServicio;
 
-    @GetMapping
-    public ResponseEntity<Page<Pedido>> getPedidos(
-            @RequestParam(required = false) Integer page,
-            @RequestParam(required = false) Integer size) {
-        if (page == null || size == null)
-            return ResponseEntity.ok(pedidoServicio.getPedidos(PageRequest.of(0, Integer.MAX_VALUE)));
-        return ResponseEntity.ok(pedidoServicio.getPedidos(PageRequest.of(page, size)));
-    }
-
-    @GetMapping("/{pedidoId}")
-    public ResponseEntity<Pedido> getPedidoById(@PathVariable Long pedidoId) {
-        Optional<Pedido> result = pedidoServicio.getPedidoById(pedidoId);
-        return result.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
-    }
-
-    @GetMapping("/{clienteId}")
-    public ResponseEntity<List<Pedido>> getPedidosPorCliente(@PathVariable Long clienteId) {
-        List<Pedido> pedidos = pedidoServicio.getPedidoPorCliente(clienteId);
-
-        if (pedidos.isEmpty()) {
-            return ResponseEntity.noContent().build();
+        @PostMapping("/comprar")
+        public void realizarCompra(@RequestBody PedidoDTO pedido) {
+           pedidoServicio.realizarComprar(pedido);
         }
-
-        return ResponseEntity.ok(pedidos);
-    }
-
-    @PostMapping
-    public void crearPedido(@RequestBody Pedido pedido) {
-        pedidoServicio.crearPedido(pedido);
-    }
-
-
 }
