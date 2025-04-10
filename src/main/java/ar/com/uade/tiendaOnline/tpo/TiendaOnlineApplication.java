2b18ec9cddccd35479623b632cda00dcfb414bae
Daniela <dggtn97@gmail.com>
2025-04-10 14:32:57 -0300

    formato

--- src/main/java/ar/com/uade/tiendaOnline/tpo/TiendaOnlineApplication.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/TiendaOnlineApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TiendaOnlineApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TiendaOnlineApplication.class, args);
 	}
 
