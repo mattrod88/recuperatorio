b24623e660c060a43e77eda82fa78f97ae8a50e9
Daniela <dggtn97@gmail.com>
2025-04-11 15:20:02 -0300

    agrego metodo de saveProductWithImage con su excepcion

--- src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/ProductoController.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/ProductoController.java
@@ -1,10 +1,16 @@
 package ar.com.uade.tiendaOnline.tpo.controllers;
 
+import ar.com.uade.tiendaOnline.tpo.entidad.Categoria;
 import ar.com.uade.tiendaOnline.tpo.entidad.Producto;
+import ar.com.uade.tiendaOnline.tpo.entidad.dto.ProductoDTO;
 import ar.com.uade.tiendaOnline.tpo.servicios.producto.IProductoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -22,7 +28,23 @@ public class ProductoController {
         }
             return productoServicio.obtenerTodosLosProductos();
         }
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadProductWithImage(@ModelAttribute ProductoDTO productDTO,
+                                                         @RequestParam("file") MultipartFile file) {
+        try {
+            Producto producto = new Producto();
+            producto.setNombre(productDTO.getNombre());
 
+            Categoria categoria = new Categoria();
+            categoria.setId(productDTO.getCategoria_id());
+            producto.setCategoria(categoria);
+
+            productoServicio.saveProductWithImage(producto, file);
+            return new ResponseEntity<>("Producto se cargo satisfactoriamente", HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<>("No se pudo cargar el producto", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 
     @PostMapping
