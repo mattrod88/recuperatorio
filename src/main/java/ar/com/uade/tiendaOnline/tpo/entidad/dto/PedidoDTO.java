310ac3bd10414468ad3d916665134b37d526ef38
Daniela <dggtn97@gmail.com>
2025-04-11 12:59:13 -0300

    tiene atributo una lista de tipo ItemPedidoDTO que se llama items

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/PedidoDTO.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/PedidoDTO.java
@@ -1,13 +1,8 @@
 package ar.com.uade.tiendaOnline.tpo.entidad.dto;
-
-import ar.com.uade.tiendaOnline.tpo.entidad.Usuario;
 import lombok.Data;
-
-import java.time.LocalDate;
 import java.util.List;
 
 @Data
 public class PedidoDTO {
-   List<ItemPedidoDTO>items;
-
+   List<ItemPedidoDTO> items;
 }
