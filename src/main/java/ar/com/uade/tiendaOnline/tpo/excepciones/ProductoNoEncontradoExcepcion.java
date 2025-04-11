2239231e1455104b4632e3b1543daa6f31e8e462
Daniela <dggtn97@gmail.com>
2025-04-11 13:00:31 -0300

    agrego excepcion para productoNoEncontrado

--- src/main/java/ar/com/uade/tiendaOnline/tpo/excepciones/ProductoNoEncontradoExcepcion.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/excepciones/ProductoNoEncontradoExcepcion.java
@@ -1,7 +1,10 @@
 package ar.com.uade.tiendaOnline.tpo.excepciones;
 
+import ar.com.uade.tiendaOnline.tpo.entidad.dto.ItemPedidoDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason ="El producto  no existe")
 public class ProductoNoEncontradoExcepcion extends RuntimeException {
-  public ProductoNoEncontradoExcepcion(String message) {
-    super(message);
-  }
+
 }
