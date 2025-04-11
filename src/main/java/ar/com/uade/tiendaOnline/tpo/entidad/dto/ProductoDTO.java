1ae7179a2d4fbf325c84878f513079ffe02849b5
Daniela <dggtn97@gmail.com>
2025-04-11 15:21:38 -0300

    agrego metodo de guardar imagen en interfaz

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/ProductoDTO.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/ProductoDTO.java
@@ -1,4 +1,9 @@
 package ar.com.uade.tiendaOnline.tpo.entidad.dto;
 
+import lombok.Data;
+
+@Data
 public class ProductoDTO {
+    String nombre;
+    Long categoria_id;
 }
--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/producto/IProductoServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/producto/IProductoServicio.java
@@ -2,7 +2,9 @@ package ar.com.uade.tiendaOnline.tpo.servicios.producto;
 
 import ar.com.uade.tiendaOnline.tpo.entidad.Categoria;
 import ar.com.uade.tiendaOnline.tpo.entidad.Producto;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface IProductoServicio {
@@ -16,4 +18,6 @@ public interface IProductoServicio {
     void eliminarProducto(Producto producto);
 
     Producto obtenerProductoPorId(Long id);
+
+    Producto saveProductWithImage(Producto producto, MultipartFile file)throws IOException;
 }
