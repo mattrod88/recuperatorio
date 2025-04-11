eeacb05a4be1c1470e3f4b7945fdd5c9a0e96420
Daniela <dggtn97@gmail.com>
2025-04-11 13:02:56 -0300

    agrego repositorio para guardar la info de los productos comprados

--- src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/ProductoCompradoRepositorio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/ProductoCompradoRepositorio.java
@@ -1,7 +1,7 @@
 package ar.com.uade.tiendaOnline.tpo.repositorio;
 
-import ar.com.uade.tiendaOnline.tpo.entidad.Categoria;
 import ar.com.uade.tiendaOnline.tpo.entidad.Producto;
+import ar.com.uade.tiendaOnline.tpo.entidad.ProductoComprado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,9 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProductoRepositorio extends JpaRepository<Producto,Long> {
-    @Query(value = "select p from Producto p where p.categoria.descripcion = ?1")
-    List<Producto> findByCategoria(String categoria) ;
+public interface ProductoCompradoRepositorio extends JpaRepository<ProductoComprado,Long> {
 
 
 
