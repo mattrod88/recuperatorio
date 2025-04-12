1997da26b780558b5990bd47445be4beefd07cd7
Daniela <dggtn97@gmail.com>
2025-04-12 09:57:55 -0300

    encontrar por email el usuario si existe o no

--- src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/UsuarioRepositorio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/UsuarioRepositorio.java
@@ -7,10 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UsuarioRepositorio extends JpaRepository<Usuario,Long> {
-    @Query(value = "select u from Usuario u where u.email = ?1")
-    boolean estaEmail(String email) ;
-    @Query(value = "select u from Usuario u where u.contrasena = ?1")
-    boolean esMismaContrasena(String contrasena);
+    Optional<Usuario> findByEmail(String mail);
+    Optional<Object> findByContrasena(String contrasena);
+
 }
