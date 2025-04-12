b7af7bf17967de1ef8cf9c0e4f11f653eb6810b3
Daniela <dggtn97@gmail.com>
2025-04-12 09:56:21 -0300

    creo excepcion que devuelva un bad request + un mensaje cuando se intentan logear con datos erroneos que no existen en la BD de usuariosRegistrados

--- src/main/java/ar/com/uade/tiendaOnline/tpo/excepciones/DatosIngresadosExcepcion.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/excepciones/DatosIngresadosExcepcion.java
@@ -1,7 +1,8 @@
 package ar.com.uade.tiendaOnline.tpo.excepciones;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.
+
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason ="No se puede iniciar sesion,error")
 public class DatosIngresadosExcepcion extends RuntimeException {
