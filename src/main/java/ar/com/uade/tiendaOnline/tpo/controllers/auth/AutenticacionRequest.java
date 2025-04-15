eddd3ffe436f80352a624a124dc4386219da7007
Daniela <dggtn97@gmail.com>
2025-04-15 16:50:25 -0300

    agrego dto de para quthenticationRequest que tenga email y contrasena

--- src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/auth/AutenticacionRequest.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/auth/AutenticacionRequest.java
@@ -1,4 +1,20 @@
-package ar.com.uade.tiendaOnline.tpo.entidad.dto;
+package ar.com.uade.tiendaOnline.tpo.controllers.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class AutenticacionRequest {
-}
+
+
+        private String email;
+        String contrasena;
+    }
+
+
