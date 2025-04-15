4344eb944c428ad5d5174c44fe802c2a3abcae0e
Daniela <dggtn97@gmail.com>
2025-04-15 17:15:22 -0300

    al metodo crear categoria le agrego un @Transactional

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/categoria/CategoriaServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/categoria/CategoriaServicio.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,7 +24,7 @@ public class CategoriaServicio implements ICategoriaServicio {
         public Optional<Categoria> getCategoriaById(Long categoryId) {
             return categoriaRepositorio.findById(categoryId);
         }
-
+        @Transactional(rollbackFor = Throwable.class)
         public Categoria crearCategoria(String descripcion) throws CategoriaDuplicateExcepcion {
             List<Categoria> categorias = categoriaRepositorio.findByDescripcion(descripcion);
             if (categorias.isEmpty())
