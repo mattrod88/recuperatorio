893c3c0ed657f4229aeac8b43eb2d345cb1c4bbd
KeilaCheca <kchecaorihuela@uade.edu.ar>
2025-04-16 18:13:50 -0300

    modifico metodo registrarse para agregar excepcion UsuarioDuplicado

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/autenticacion/AutenticacionServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/autenticacion/AutenticacionServicio.java
@@ -3,6 +3,7 @@ import ar.com.uade.tiendaOnline.tpo.controllers.auth.AuthenticationResponse;
 import ar.com.uade.tiendaOnline.tpo.controllers.config.JwtService;
 import ar.com.uade.tiendaOnline.tpo.entidad.Roles;
 import ar.com.uade.tiendaOnline.tpo.entidad.Usuario;
+import ar.com.uade.tiendaOnline.tpo.excepciones.UsuarioDuplicado;
 import ar.com.uade.tiendaOnline.tpo.controllers.auth.AutenticacionRequest;
 import ar.com.uade.tiendaOnline.tpo.controllers.auth.RegistrarseRequest;
 import ar.com.uade.tiendaOnline.tpo.repositorio.UsuarioRepositorio;
@@ -21,20 +22,28 @@ import org.springframework.security.crypto.password.PasswordEncoder;
         private final AuthenticationManager authenticationManager;
 
         public AuthenticationResponse registrarse(RegistrarseRequest request) {
-            var usuario = Usuario.builder()
-                    .nombre(request.getNombre())
-                    .apellido(request.getApellido())
-                    .email(request.getEmail())
-                    .contrasena(passwordEncoder.encode(request.getContrasena()))
-                    .rol(Roles.CLIENTE)  //por default todos son clientes excepto el admin, el admin va cargado en la base dato por SQL admin.
-                    .build();
-
-            repository.save(usuario);
-            var jwtToken = jwtService.generateToken(usuario,Roles.CLIENTE);
-            return AuthenticationResponse.builder()
-                    .accessToken(jwtToken)
-                    .build();
+        var usuarioExistente = repository.findByEmail(request.getEmail());
+        if (usuarioExistente.isPresent()) {
+                throw new UsuarioDuplicado(); 
         }
+        
+        var usuario = Usuario.builder()
+                .nombre(request.getNombre())
+                .apellido(request.getApellido())
+                .email(request.getEmail())
+                .contrasena(passwordEncoder.encode(request.getContrasena()))
+                .rol(Roles.CLIENTE)  //por defecto todos son clientes
+                .build();
+
+        repository.save(usuario);
+        
+        var jwtToken = jwtService.generateToken(usuario, Roles.CLIENTE);
+        
+        return AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                .build();
+        }
+
 
         public AuthenticationResponse authenticarse(AutenticacionRequest request) {
             authenticationManager.authenticate(
