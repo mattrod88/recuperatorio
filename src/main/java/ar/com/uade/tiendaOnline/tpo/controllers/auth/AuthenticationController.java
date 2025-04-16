08c5672bca1e6bf08c48df74daf87612860304ee
KeilaCheca <kchecaorihuela@uade.edu.ar>
2025-04-16 18:15:19 -0300

    agrego en registrarse la excepcion de usuarioDuplicado

--- src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/auth/AuthenticationController.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/auth/AuthenticationController.java
@@ -5,6 +5,8 @@ import ar.com.uade.tiendaOnline.tpo.entidad.dto.UsuarioRequest;
 import ar.com.uade.tiendaOnline.tpo.excepciones.UsuarioDuplicado;
 import ar.com.uade.tiendaOnline.tpo.servicios.autenticacion.AutenticacionServicio;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,10 +21,17 @@ public class AuthenticationController {
     private final AutenticacionServicio service;
 
     @PostMapping("/registrarse")
-    public ResponseEntity<AuthenticationResponse> registrarse(
-            @RequestBody RegistrarseRequest request) {
-        return ResponseEntity.ok(service.registrarse(request));
-    }
+    public ResponseEntity<?> registrarse(@RequestBody RegistrarseRequest request) { // Usamos ResponseEntity<?> porque el tipo de respuesta puede ser variable
+                                                                                    // Si el registro es exitoso, retorna AuthenticationResponse
+                                                                                    // Si hay un error, retorna un String con el mensaje de error.
+        try {                                                                       
+            AuthenticationResponse response = service.registrarse(request);
+            return ResponseEntity.ok(response); 
+        } catch (UsuarioDuplicado e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                 .body("El mail que se intenta agregar ya está registrado");
+        }
+    } 
 
     @PostMapping("/autenticarse")
     public ResponseEntity<AuthenticationResponse> autenticarse(
