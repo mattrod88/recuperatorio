0716c6a3e8bb0d6b35db9732dab670eec77f343d
Daniela <dggtn97@gmail.com>
2025-04-21 19:35:01 -0300

    agrego los parametros PedidoDTO pedido, Usuario usuario a la interfaz de pedidoServicio

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/pedido/IPedidoServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/pedido/IPedidoServicio.java
@@ -2,13 +2,14 @@ package ar.com.uade.tiendaOnline.tpo.servicios.pedido;
 
 import ar.com.uade.tiendaOnline.tpo.entidad.Pedido;
 import ar.com.uade.tiendaOnline.tpo.entidad.Producto;
+import ar.com.uade.tiendaOnline.tpo.entidad.Usuario;
 import ar.com.uade.tiendaOnline.tpo.entidad.dto.ItemPedidoDTO;
 import ar.com.uade.tiendaOnline.tpo.entidad.dto.PedidoDTO;
 
 import java.util.List;
 
 public interface IPedidoServicio {
-    void realizarComprar(PedidoDTO pedido);
+    void realizarComprar(PedidoDTO pedido, Usuario usuario);
 
 
     //void hacerPedido(Pedido pedido);
