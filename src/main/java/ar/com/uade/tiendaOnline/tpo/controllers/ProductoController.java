34a088fcd8b66149700cbc5ba24f39b74f4a9665
Daniela <dggtn97@gmail.com>
2025-04-21 16:50:23 -0300

    metodo para actualizar producto

--- src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/ProductoController.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/ProductoController.java
@@ -1,8 +1,9 @@
 package ar.com.uade.tiendaOnline.tpo.controllers;
 
-import ar.com.uade.tiendaOnline.tpo.entidad.Categoria;
+import ar.com.uade.tiendaOnline.tpo.entidad.Imagen;
 import ar.com.uade.tiendaOnline.tpo.entidad.Producto;
 import ar.com.uade.tiendaOnline.tpo.entidad.dto.ProductoDTO;
+import ar.com.uade.tiendaOnline.tpo.excepciones.ProductoInexistenteExcepcion;
 import ar.com.uade.tiendaOnline.tpo.servicios.producto.IProductoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,21 +25,21 @@ public class ProductoController {
 
     @GetMapping
     public List<Producto> obtenerProductosXcategoria(@RequestParam(required = false) String categoria) {
-        if(categoria!=null) {
+        if (categoria != null) {
             return productoServicio.obtenerProductosXCategoria(categoria);
         }
-            return productoServicio.obtenerTodosLosProductos();
+        return productoServicio.obtenerTodosLosProductos();
     }
 
     @PostMapping("{id}/imagen")
-    public ResponseEntity<String>  subirImagen(@PathVariable Long id, @RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> subirImagen(@PathVariable Long id, @RequestParam("file") MultipartFile file) {
         try {
             Producto producto = new Producto();
             producto.setId(id);
-            productoServicio.guardarImagen(producto, file);
-            return new ResponseEntity<>("Producto cargado con exito", HttpStatus.OK);
+            productoServicio.subirImagen(producto, file);
+            return new ResponseEntity<>("Imagen cargada con exito", HttpStatus.OK);
         } catch (IOException e) {
-            return new ResponseEntity<>("No se pudo cargar el producto", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("No se pudo cargar la imagen", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -51,14 +53,29 @@ public class ProductoController {
     public Producto clickEnUnProducto(@PathVariable Long id) {
 
         return productoServicio.obtenerProductoPorId(id);
-}
+    }
+
+    @GetMapping("/{id}/imagenes")
+    public ArrayList<Imagen> obtenerImagenesDeUnProducto(@PathVariable Long id) {
+
+        return productoServicio.obtenerImagenes(id);
+    }
 
 
-
-    @DeleteMapping("delete/{id}")
+    @DeleteMapping("/{id}")
     public void eliminarProducto(@PathVariable Long id) {
 
         productoServicio.eliminarProducto(id);
 
-}
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> actualizarProducto(@PathVariable Long id, @RequestBody ProductoDTO productoDTO) {
+        try {
+            productoServicio.actualizarProducto(id, productoDTO);
+            return ResponseEntity.ok().build();
+        } catch (ProductoInexistenteExcepcion e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
