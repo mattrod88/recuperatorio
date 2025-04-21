bade7dfca1fd2e084fc61cd70f04ab26d664c3f3
Daniela <dggtn97@gmail.com>
2025-04-21 16:58:24 -0300

    agrego metodo para actualizarCategoria por Nombre y tambien para saber si existe la categoria buscandolo por nombre

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/categoria/CategoriaServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/categoria/CategoriaServicio.java
@@ -1,6 +1,11 @@
 package ar.com.uade.tiendaOnline.tpo.servicios.categoria;
 import ar.com.uade.tiendaOnline.tpo.entidad.Categoria;
+import ar.com.uade.tiendaOnline.tpo.entidad.Producto;
+import ar.com.uade.tiendaOnline.tpo.entidad.dto.CategoriaDTO;
+import ar.com.uade.tiendaOnline.tpo.entidad.dto.ProductoDTO;
 import ar.com.uade.tiendaOnline.tpo.excepciones.CategoriaDuplicateExcepcion;
+import ar.com.uade.tiendaOnline.tpo.excepciones.CategoriaInexistenteExcepcion;
+import ar.com.uade.tiendaOnline.tpo.excepciones.ProductoInexistenteExcepcion;
 import ar.com.uade.tiendaOnline.tpo.repositorio.CategoriaRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -31,5 +36,32 @@ public class CategoriaServicio implements ICategoriaServicio {
                 return categoriaRepositorio.save(new Categoria(descripcion));
             throw new CategoriaDuplicateExcepcion();
         }
+
+    @Override
+    @Transactional(rollbackFor = Throwable.class)
+    public void actualizarCategoria(Long id, CategoriaDTO categoriaDTO) throws CategoriaInexistenteExcepcion {
+
+        if (id == null || categoriaDTO == null) {
+            throw new IllegalArgumentException("Parámetros inválidos para actualizar categoria");
+        }
+
+        Categoria categoriaExistente = categoriaRepositorio.findById(id)
+                .orElseThrow(CategoriaInexistenteExcepcion::new);
+
+
+        if (categoriaDTO.getDescripcion() != null && !categoriaDTO.getDescripcion().trim().isEmpty()
+                && !categoriaDTO.getDescripcion().equals(categoriaExistente.getDescripcion())) {
+
+            if (existeCategoriaConNombre(categoriaDTO.getDescripcion())) {
+                throw new IllegalArgumentException("Ya existe una categoria con el nombre: " + categoriaDTO.getDescripcion());
+            }
+            categoriaExistente.setDescripcion(categoriaDTO.getDescripcion());
+        }
+
+    }
+
+    private boolean existeCategoriaConNombre(String nombre) {
+        return !categoriaRepositorio.findByDescripcion(nombre).isEmpty();
+    }
 
 }
