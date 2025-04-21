fbe36da93fb8a40c4505ea2947fee1516616ec53
Daniela <dggtn97@gmail.com>
2025-04-21 16:57:34 -0300

    metodos para registrarse y autenticarse(logearse)

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/autenticacion/IAutenticacion.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/autenticacion/IAutenticacion.java
@@ -3,6 +3,8 @@ package ar.com.uade.tiendaOnline.tpo.servicios.autenticacion;
 import ar.com.uade.tiendaOnline.tpo.controllers.auth.AutenticacionRequest;
 import ar.com.uade.tiendaOnline.tpo.controllers.auth.AuthenticationResponse;
 import ar.com.uade.tiendaOnline.tpo.controllers.auth.RegistrarseRequest;
+import org.springframework.stereotype.Service;
+
 
 public interface IAutenticacion {
 
