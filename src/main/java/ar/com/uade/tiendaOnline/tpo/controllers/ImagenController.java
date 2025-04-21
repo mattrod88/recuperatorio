b4dcbbb0dadf67e9b751b5d1fa89c60462402f51
Daniela <dggtn97@gmail.com>
2025-04-21 16:48:39 -0300

    creo controller de imagen que permita borrar imagen por id

--- src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/ImagenController.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/controllers/ImagenController.java
@@ -1,4 +1,23 @@
 package ar.com.uade.tiendaOnline.tpo.controllers;
 
+import ar.com.uade.tiendaOnline.tpo.entidad.Imagen;
+import ar.com.uade.tiendaOnline.tpo.entidad.Producto;
+import ar.com.uade.tiendaOnline.tpo.excepciones.ProductoInexistenteExcepcion;
+import ar.com.uade.tiendaOnline.tpo.servicios.imagen.IImagenServicio;
+import ar.com.uade.tiendaOnline.tpo.servicios.imagen.ImagenServicio;
+import ar.com.uade.tiendaOnline.tpo.servicios.producto.IProductoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+@RestController
+@RequestMapping("/imagenes")
 public class ImagenController {
+    @Autowired
+    private IImagenServicio imagenServicio;
+
+ @DeleteMapping("/{id}")
+    public void eliminarImagenPorId(@PathVariable Long id) {
+     imagenServicio.delete(id);
+    }
 }
