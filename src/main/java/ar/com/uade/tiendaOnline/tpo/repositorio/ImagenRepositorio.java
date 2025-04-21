95f68d8c079cecea406ca64624d585ee239fba20
Daniela <dggtn97@gmail.com>
2025-04-21 16:56:20 -0300

    creo repositorio para guardar imagens y buscar imagenes en la BD

--- src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/ImagenRepositorio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/repositorio/ImagenRepositorio.java
@@ -1,4 +1,16 @@
 package ar.com.uade.tiendaOnline.tpo.repositorio;
 
-public class ImagenRepositorio {
+import ar.com.uade.tiendaOnline.tpo.entidad.Imagen;
+import ar.com.uade.tiendaOnline.tpo.entidad.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+@Repository
+public interface ImagenRepositorio extends JpaRepository<Imagen,Long> {
+
+    @Query(value = "select i from Imagen i where i.producto.id = ?1 ")
+    ArrayList<Imagen> obtenerImagenesDeUnProducto(Long id);
+
 }
