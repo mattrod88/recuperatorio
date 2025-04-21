1e4e77a6dd4f5427086d5677d7aba55d167a6f52
Daniela <dggtn97@gmail.com>
2025-04-21 17:00:03 -0300

    creo clase ImagenServicio e implemento metodos de la interfaz

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/imagen/ImagenServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/imagen/ImagenServicio.java
@@ -1,4 +1,20 @@
 package ar.com.uade.tiendaOnline.tpo.servicios.imagen;
 
-public class ImagenServicio {
+import ar.com.uade.tiendaOnline.tpo.entidad.Imagen;
+import ar.com.uade.tiendaOnline.tpo.repositorio.ImagenRepositorio;
+import org.hibernate.annotations.SecondaryRow;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImagenServicio implements  IImagenServicio{
+    @Autowired
+    private ImagenRepositorio imagenRepositorio;
+    @Override
+    public void delete(Long id) {
+        Imagen i = new Imagen();
+        i.setId(id);
+        imagenRepositorio.delete(i);
+
+    }
 }
