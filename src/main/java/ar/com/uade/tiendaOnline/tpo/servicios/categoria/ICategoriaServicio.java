dfa3fe00734410ef9f9196cc72ab701d0bbd2e71
Daniela <dggtn97@gmail.com>
2025-04-21 16:58:39 -0300

    agrego metodo para actualizarCategoria por Nombre y tambien para saber si existe la categoria buscandolo por nombre

--- src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/categoria/ICategoriaServicio.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/servicios/categoria/ICategoriaServicio.java
@@ -2,14 +2,15 @@ package ar.com.uade.tiendaOnline.tpo.servicios.categoria;
 
 
 import ar.com.uade.tiendaOnline.tpo.entidad.Categoria;
+import ar.com.uade.tiendaOnline.tpo.entidad.dto.CategoriaDTO;
 import ar.com.uade.tiendaOnline.tpo.excepciones.CategoriaDuplicateExcepcion;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-
+import org.springframework.stereotype.Service;
 
 
 import java.util.Optional;
-
+@Service
 public interface ICategoriaServicio  {
     Page<Categoria> getCategorias(PageRequest pageRequest);
 
@@ -17,5 +18,7 @@ public interface ICategoriaServicio  {
     Optional<Categoria> getCategoriaById(Long categoryId);
 
     Categoria crearCategoria(String description) throws CategoriaDuplicateExcepcion;
+
+    void actualizarCategoria(Long id, CategoriaDTO categoriaDTO);
 }
 
