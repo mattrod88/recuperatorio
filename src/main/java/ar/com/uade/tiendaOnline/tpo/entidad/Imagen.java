7f861e581c2f3c35489fe030650490abb38f77dd
Daniela <dggtn97@gmail.com>
2025-04-21 16:52:20 -0300

    Agrego todos sus atributos a la entidad imagen y la relacion manyToOne con producto

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/Imagen.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/Imagen.java
@@ -1,4 +1,26 @@
 package ar.com.uade.tiendaOnline.tpo.entidad;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "imagenes")
 public class Imagen {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String imagenNombre;
+
+    @Lob
+    private byte[] imagenData;
+
+    @Column
+    private String descripcion;
+
+    @ManyToOne
+    private Producto producto;
 }
