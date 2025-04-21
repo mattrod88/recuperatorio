3f853569d4005f06aa045da532085cffca16905e
Daniela <dggtn97@gmail.com>
2025-04-21 16:55:51 -0300

    arreglo mensaje

--- src/main/java/ar/com/uade/tiendaOnline/tpo/excepciones/CategoriaInexistenteExcepcion.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/excepciones/CategoriaInexistenteExcepcion.java
@@ -1,7 +1,8 @@
 package ar.com.uade.tiendaOnline.tpo.excepciones;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "La categoria que buscas no existe")
 public class CategoriaInexistenteExcepcion extends RuntimeException {
-  public CategoriaInexistenteExcepcion(String message) {
-    super(message);
-  }
+
 }
