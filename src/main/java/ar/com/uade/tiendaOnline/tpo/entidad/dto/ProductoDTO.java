c0f20da521b67f4145457137831c28d0a9a9bfd1
Daniela <dggtn97@gmail.com>
2025-04-21 16:55:01 -0300

    agrego todos los atributos que necesita para despues poder ser actualizados como nombre, categoria_id,cantidad y precio

--- src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/ProductoDTO.java
+++ src/main/java/ar/com/uade/tiendaOnline/tpo/entidad/dto/ProductoDTO.java
@@ -1,9 +1,12 @@
 package ar.com.uade.tiendaOnline.tpo.entidad.dto;
 
+import jakarta.persistence.Column;
 import lombok.Data;
 
 @Data
 public class ProductoDTO {
     String nombre;
     Long categoria_id;
+    int cantidad;
+    double precio;
 }
