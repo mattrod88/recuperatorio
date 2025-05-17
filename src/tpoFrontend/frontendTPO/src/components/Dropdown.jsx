be56c22de867db37832ba7bbdfd4487250baba59
Daniela <dggtn97@gmail.com>
2025-05-17 19:09:27 -0300

    agrego carpeta de archivo frontend del TPO

---
+++ src/tpoFrontend/frontendTPO/src/components/Dropdown.jsx
@@ -0,0 +1,150 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const Dropdown = ({ items }) => {
+  const toggle = () => {
+    setAbierto(!abierto)
+
+  }
+  const [abierto, setAbierto] = useState(false);
+  const activeStyle = "underline underline-offset-4";
+  return (
+    <>
+      <div>
+        <button
+          type="button"
+          className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50"
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
+          onClick={toggle}
+        >
+          Categorias
+          <svg
+            className="-mr-1 size-5 text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+            data-slot="icon"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+      <div
+        className="absolute z-10 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-hidden"
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="menu-button"
+        tabIndex="-1"
+      >
+        {abierto && (
+          <div className="py-1" role="none">
+            {items.map((item, indice) => (
+              <a
+                key={indice}
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-700"
+                role="menuitem"
+                tabIndex="-1"
+                id="menu-item-"
+                indice
+              >
+                <NavLink
+                  to={item.link}
+                  className={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                  }
+                >
+                  {item.titulo}
+                </NavLink>
+              </a>
+            ))}
+          </div>
+        )}
+
+        {/* <div className="py-1" role="none">
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700"
+            role="menuitem"
+            tabIndex="-1"
+            id="menu-item-0"
+          >
+            <NavLink
+              to="/categoria/frutas_secas"
+              className={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Frutos Secos
+            </NavLink>
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700"
+            role="menuitem"
+            tabindex="-1"
+            id="menu-item-1"
+          >
+            <NavLink
+              to="/categoria/lacteos"
+              className={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Lacteos
+            </NavLink>
+          </a>
+        </div>
+        <div className="py-1" role="none">
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700"
+            role="menuitem"
+            tabindex="-1"
+            id="menu-item-2"
+          >
+            <NavLink
+              to="/categoria/bebidas"
+              className={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Bebidas
+            </NavLink>
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700"
+            role="menuitem"
+            tabindex="-1"
+            id="menu-item-3"
+          >
+            <NavLink
+              to="/categoria/especias"
+              className={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Especies
+            </NavLink>
+          </a>
+        </div>
+        <div className="py-1" role="none">
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700"
+            role="menuitem"
+            tabindex="-1"
+            id="menu-item-4"
+          >
+            <NavLink
+              to="/categoria/frutas"
+              className={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Frutas
+            </NavLink>
+          </a>
+        </div> */}
+      </div>
+    </>
+  );
+};
+export default Dropdown;
