2716fb3adb5d9208937ceb204f17bef6b8768b7c
pedroberro <berrotaran.pedro@gmail.com>
2025-05-22 16:56:55 -0300

    agrego componente carrito y carritoContexto

---
+++ src/tpoFrontend/frontendTPO/src/context/CarritoContext.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, useState } from 'react';
+
+export const ContextoCarrito = createContext();
+
+export function ProveedorCarrito({ hijos }) {
+  const [carrito, setCarrito] = useState([]);
+
+  const agregarAlCarrito = (producto, cantidad = 1) => {
+    setCarrito(anterior => {
+      const existe = anterior.find(item => item.producto.id === producto.id);
+      if (existe) {
+        return anterior.map(item =>
+          item.producto.id === producto.id
+            ? { ...item, cantidad: item.cantidad + cantidad }
+            : item
+        );
+      }
+      return [...anterior, { producto, cantidad }];
+    });
+  };
+
+  const eliminarDelCarrito = id => {
+    setCarrito(anterior => anterior.filter(item => item.producto.id !== id));
+  };
+
+  const vaciarCarrito = () => setCarrito([]);
+
+  return (
+    <ContextoCarrito.Provider value={{ carrito, agregarAlCarrito, eliminarDelCarrito, vaciarCarrito }}>
+      {hijos}
+    </ContextoCarrito.Provider>
+  );
+}
