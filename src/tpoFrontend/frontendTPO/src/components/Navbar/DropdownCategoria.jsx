5117d1e5ae6e867eafc0c1306b080bcb42514e60
Daniela <dggtn97@gmail.com>
2025-05-24 16:57:18 -0300

    mejoro componente navbar lo hago responsive uso tailwind flowbite e iconos svg bootstrap

---
+++ src/tpoFrontend/frontendTPO/src/components/Navbar/DropdownCategoria.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const DropdownCategoria = ({ items }) => {
+  const toggle = () => {
+    setAbierto(!abierto);
+  };
+  const [abierto, setAbierto] = useState(false);
+  const activeStyle = "underline underline-offset-4";
+  return (
+    <>
+      <button
+        id="dropdownNavbarLink"
+        data-dropdown-toggle="dropdownNavbar"
+        className="flex items-center justify-between w-full py-2 px-3 text-lime-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-lime-900 md:w-auto dark:text-lime-900 md:dark:hover:text-lime-900 dark:focus:text-lime-900 dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+      >
+        Categoria{" "}
+        <svg
+          className="w-2.5 h-2.5 ms-2.5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m1 1 4 4 4-4"
+          />
+        </svg>
+      </button>
+      {abierto && (
+        <div
+          id="dropdownNavbar"
+          className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-green-lime-900"
+        >
+          <ul
+            className="py-2 text-sm text-gray-700 dark:text-lime-900"
+            aria-labelledby="dropdownLargeButton"
+          >
+            <li>
+              {items.map((item, indice) => (
+                <a
+                  key={indice}
+                  href="#"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:green-lime-900 dark:hover:text-lime-900"
+                  role="menuitem"
+                  tabIndex="-1"
+                  id="menu-item-"
+                  indice
+                  onClick={toggle}
+                >
+                  <NavLink
+                    to={item.link}
+                    className={({ isActive }) =>
+                      isActive ? activeStyle : undefined
+                    }
+                  >
+                    {item.titulo}
+                  </NavLink>
+                </a>
+              ))}
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
+export default DropdownCategoria;
