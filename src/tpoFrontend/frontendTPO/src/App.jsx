941c1bcf60fc3440db413e8bcee21415cf5fd65f
Daniela <dggtn97@gmail.com>
2025-05-24 16:57:41 -0300

    mejoro componente navbar lo hago responsive uso tailwind flowbite e iconos svg bootstrap

--- src/tpoFrontend/frontendTPO/src/App.jsx
+++ src/tpoFrontend/frontendTPO/src/App.jsx
@@ -1,29 +1,17 @@
 import './tailwind.css'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import ElFooter from './components/Footer';
-import Home from './views/Home';
-import ListaDeProductos from './components/Catalogo';
-import DetalleProducto from './components/DetalleProducto';
-import Carrito from './components/Carrito';
-import { ProveedorCarrito } from './contexts/CarritoContexto';
-
+import Header from './components/Navbar/Header'
+import Footer from './components/Footer/Footer';
+import { Rutas } from './Rutas/Todas';
 function App() {
   return (
-    <ProveedorCarrito>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/categoria/:nombre_categoria" element={<ListaDeProductos />} />
-          <Route path="/producto/:id" element={<DetalleProducto />} />
-          <Route path="/carrito" element={<Carrito />} />
-        </Routes>
-        <ElFooter />
-      </BrowserRouter>
-    </ProveedorCarrito>
+<div className='App'>
+<Header/>
+<Rutas/>
+<Footer/>
+</div>
+
   );
 }
 
