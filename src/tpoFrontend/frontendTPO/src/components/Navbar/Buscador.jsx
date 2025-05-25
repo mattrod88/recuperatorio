40c377251afb3d4a59aa39e74042309503d174e5
Daniela <dggtn97@gmail.com>
2025-05-25 17:00:25 -0300

    agrego en navbar input name del buscador

--- src/tpoFrontend/frontendTPO/src/components/Navbar/Buscador.jsx
+++ src/tpoFrontend/frontendTPO/src/components/Navbar/Buscador.jsx
@@ -8,7 +8,7 @@ export default function Buscador() {
         <form  className="flex items-center">   
             <div className="relative w-full">
                 <span className="bi bi-search flex absolute inset-y-0 left-0 items-center pl-3  "></span>
-                <input  name="search" type="text" id="simple-search" className=" text-lime-900 text-xl rounded-lg focus:ring-lime-500 focus:border-lime-500 block w-full pl-10 p-2.5  dark:placeholder-lime-900 dark:text-LIME-900 dark:focus:ring-lime-500 dark:focus:border-lime-500" placeholder="Escribí acá lo que estás buscando" autoComplete="off"  />
+                <input  name="buscar" type="text" id="simple-search" className=" text-lime-900 text-xl rounded-lg focus:ring-lime-500 focus:border-lime-500 block w-full pl-10 p-2.5  dark:placeholder-lime-900 dark:text-LIME-900 dark:focus:ring-lime-500 dark:focus:border-lime-500" placeholder="Escribí acá lo que estás buscando" autoComplete="off"  />
             </div>
             <button type="submit" className="bi bi-search py-2.5 px-3 ml-2 text-sm font-semibold text-lime-900 bg-lime-00 rounded-lg border border-lime-700 hover:bg-lime-800 focus:ring-4 focus:outline-none focus:ring-lime-300 dark:bg-lime-600 dark:hover:bg-lime-700 dark:focus:ring-lime-800">BUSCAR
             </button>
