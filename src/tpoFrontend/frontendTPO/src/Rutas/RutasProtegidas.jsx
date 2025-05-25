37b0b9095fc94837dab4723f4ed1b81eb613784a
Daniela <dggtn97@gmail.com>
2025-05-25 16:05:06 -0300

    comento funcionalidad de componente de rutas protegidas

---
+++ src/tpoFrontend/frontendTPO/src/Rutas/RutasProtegidas.jsx
@@ -0,0 +1,7 @@
+// import React from 'react'
+// import { Navigate } from 'react-router-dom'
+
+// export const RutasProtegidas = ({children})=> {
+//     const access_token = JSON.parse(sessionStorage.storage.getProducto("access_token"))
+//   return access_token ? children : <Navigate to = "/login"/>
+// }
--- src/tpoFrontend/frontendTPO/src/Rutas/Todas.jsx
+++ src/tpoFrontend/frontendTPO/src/Rutas/Todas.jsx
@@ -1,26 +1,32 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import  Inicio  from "../pages/Inicio/Inicio";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import Inicio from "../pages/Inicio/Inicio";
 
 import DetalleProducto from "../pages/detalleProducto/DetalleProducto";
 import { ListaProductos } from "../pages/Productos/ListaProductos";
 import Auth from "../pages/Sesion/Auth";
-import Carrito from "../pages/carrito/carrito";
+import Carrito from "../pages/carrito/Carrito";
 
 
 export const Rutas = () => {
+  const access_token = true;
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/productos" element={<ListaProductos/>} />
-          <Route path="/productos/:id" element={<DetalleProducto/>} />
-          <Route path="/productos/:categoria" element={<ListaProductos/>} />
-          <Route path="/sesion" element={<Auth/>} />
-          <Route path="/carrito" element={<Carrito/>}/>
-          
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/productos" element={<ListaProductos />} />
+        <Route path="/productos/:id" element={<DetalleProducto />} />
+        <Route path="/productos/:categoria" element={<ListaProductos />} />
+        <Route path="/sesion" element={<Auth />} />
+        <Route path="/sesion" element={<Carrito />} />
+        {/* <Route
+          path="/carrito"
+          element={
+            <RutasProtegidas>
+              <Carrito />
+            </RutasProtegidas>
+          } */}
+      
+      </Routes>
+    </BrowserRouter>
   );
 };
