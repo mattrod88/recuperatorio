f960f46b31dc787dad503fb921c16eafd0774306
pedroberro <berrotaran.pedro@gmail.com>
2025-05-29 18:38:13 -0300

    elimino boton innecesario

--- src/tpoFrontend/frontendTPO/src/Admin/SideBar.jsx
+++ src/tpoFrontend/frontendTPO/src/Admin/SideBar.jsx
@@ -106,13 +106,6 @@ export default function SideBar( ) {
                 <div>
                   <ul>
                     <li>
-                      <button>
-                        <h1 className="block px-4 py-2 hover:bg-gray-100 dark:hover:green-lime-900 dark:hover text-lime-900 ">
-                          Settings
-                        </h1>
-                      </button>
-                    </li>
-                    <li>
                       <button
                         type="submit"
                         className="w-full text-white bg-lime-700 hover:bg-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-lime-600 dark:hover:bg-lime-700"
